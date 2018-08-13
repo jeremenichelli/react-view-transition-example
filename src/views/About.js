f4d1685fefe35dd21bca337d23fadfeed82573a9
Jere Menichelli <jmenichelli@gmail.com>
2018-08-14 00:01:21 +0200

    Put initial content for About page 🔍

--- src/views/About.js
+++ src/views/About.js
@@ -2,14 +2,25 @@ import React, { Component } from 'react';
 
 class About extends Component {
   render() {
-    return <div className="view">
+    return (
+      <div className="view">
         <h1>About</h1>
         <div className="content">
           <div className="content--inner">
-            Application created to showcase multiple transitions in a React application based on the route is entering. If you want to know more about this approach check this links:
+            Application created to showcase multiple transitions in React projects, based on the route is entering. If you want to know more about this approach check this links:
+            <ul>
+              <li>
+                Talks where this project is mentioned:
+                <ul>
+                  <li>A sleek view transitions recipes for web apps<br/><em>Link coming soon...</em></li>
+                  <li>Insights from building rich animated experiences in React<br/><em>Link coming soon...</em></li>
+                </ul>
+              </li>
+            </ul>
           </div>
         </div>
-      </div>;
+      </div>
+    );
   }
 }
 
