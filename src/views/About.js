de348f7b9db088c1112cf7bca8b4eca24b40ed1b
Jere Menichelli <jmenichelli@gmail.com>
2018-12-10 22:52:54 +0100

    Update with talk links, style fixes

--- src/views/About.js
+++ src/views/About.js
@@ -7,15 +7,15 @@ class About extends Component {
         <h1>About</h1>
         <div className="content">
           <div className="content--inner">
-            Application created to showcase multiple transitions in React projects, based on the route is entering. If you want to know more about this approach check this links:
+            Application created to showcase multiple transitions in React projects, based on the route is entering. If you want to know more about this approach check these links below.
+            <h2>Talks where this project is showcased</h2>
             <ul>
-              <li>
-                Talks where this project is mentioned:
-                <ul>
-                  <li>A sleek view transitions recipes for web apps<br/><em>Link coming soon...</em></li>
-                  <li>Insights from building rich animated experiences in React<br/><em>Link coming soon...</em></li>
-                </ul>
-              </li>
+              <li>A sleek view transitions recipes for web apps at <a href="https://www.youtube.com/watch?v=BgeOAzXotbY&t=3s">Real World React</a> and <a href="https://www.youtube.com/watch?v=srQP3uqMo_M&t=54s">webcamp Zagreb 2018</a></li>
+              <li>Insights from building rich animated experiences in React at <a href="https://www.youtube.com/watch?v=S3u-ccn4PEM">Byteconf React</a> and <a href="https://www.youtube.com/watch?v=tKsaIU0YqLk&t=26608s">React Day Berlin 2018</a></li>
+            </ul>
+            <h2>Posts this project is mentioned</h2>
+            <ul>
+              <li><em>Coming soon...</em></li>
             </ul>
           </div>
         </div>
--- src/views/Author.js
+++ src/views/Author.js
@@ -8,12 +8,10 @@ class Author extends Component {
         <div className="content">
           <div className="content--inner">
             <p className="content--excerpt">This project was made by Jeremias Menichelli</p>
-            <p>His interest for web development started back when he opened a website for the first time as a child and asked himself why it was so slow.</p>
-            <p>He is now an adult, or pretending to be one, and a self-taught front end engineer with a varied experience in different types of projects and workflows for web based solutions.</p>
-            <p>He initially worked in content based web and marketing sites for companies like Microsoft, State Farm, Razorfish, NYSE and others.</p>
-            <p>Later, he helped build and maintain large applications for e-commerce companies and start ups based in the United States, with focus on test driven development and scalability.</p>
-            <p>Previous to his current position, he worked for Jam3 agency building rich animated web experiences for clients like Pixar, Google, Oreo, Disney and Facebook, always dangerously obsessed with performance and best practices.</p>
-            <p>He is currently part of the Design System team at Typeform and he’s a member of webpack team. He enjoys writing articles and giving talks to share his learnings along the way.</p>
+            <p>Born and raised in Argentina, and currently living in Barcelona, his interest for web development started back when he opened a website for the first time as a child and asked himself why it was so slow.</p>
+            <p>He initially worked in content based web and marketing sites for companies like Microsoft and State Farm, and later helped build and maintain large applications for start ups based in the United States with a focus on testing and scalability.</p>
+            <p>Previous to his current position, he worked for Jam3 agency building rich animated web experiences for clients like Pixar, Google, Oreo and Facebook.</p>
+            <p>He is currently part of the Design System team at Typeform and member of webpack documentation team. He also enjoys writing articles and talks to share his learnings along the way.</p>
           </div>
         </div>
       </div>
